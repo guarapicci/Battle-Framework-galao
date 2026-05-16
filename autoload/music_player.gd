d6acbb21d8258faaa6a23daeaad6f51f34606301
felipechoi <none@nowhere>
2026-05-16 13:44:12 -0300

    A little more documentation on music player singleton

--- autoload/music_player.gd
+++ autoload/music_player.gd
@@ -1,3 +1,10 @@
+# author: (TheBuddyAdrian?)
+
+## This is a default audio player, but a singleton.
+## It includes shorthands for some commonly used audio files.
+## To play a sound file on it, call play_track with a resource path to a sound file;
+## To stop the sound, call play_track(null)
+
 extends AudioStreamPlayer
 
 const MAIN_MENU = preload("res://assets/audio/bgm/main_menu.mp3")
